bbbb5167d5bac37d259350a349b42ea7a8c11cc1
ezgisariyildiz <ezgisariyildiz0@gmail.com>
2025-01-10 18:31:09 +0300

    reverseVowels problem is solved

--- src/main/java/ReverseVowelsOfString.java
+++ src/main/java/ReverseVowelsOfString.java
@@ -5,6 +5,8 @@ public class ReverseVowelsOfString {
         char[] chars = s.toCharArray();
 
         // Sesli harfleri kontrol etmek için iki pointer oluşturuyoruz
+        // left: Baştan başlayarak sesli harf arar
+        // right: Sondan başlayarak sesli harf arar
         int left = 0, right = chars.length - 1;
 
         // Sesli harfleri tanımlıyoruz
@@ -13,10 +15,12 @@ public class ReverseVowelsOfString {
         // Sol ve sağ pointerlar birbiriyle kesişene kadar devam ediyoruz
         while (left < right) {
             // Sol pointer sesli harf bulana kadar ilerler
+            // Eğer chars[left] sesli harf değilse, left bir sonraki karaktere geçer
             while (left < right && vowels.indexOf(chars[left]) == -1) {
                 left++;
             }
             // Sağ pointer sesli harf bulana kadar ilerler
+            // Eğer chars[right] sesli harf değilse, right bir önceki karaktere geçer
             while (left < right && vowels.indexOf(chars[right]) == -1) {
                 right--;
             }
@@ -26,7 +30,7 @@ public class ReverseVowelsOfString {
             chars[left] = chars[right];
             chars[right] = temp;
 
-            // Pointerları hareket ettiriyoruz
+            // // Yer değişimi yaptıktan sonra, bir sonraki sesli harfi kontrol etmek için pointer'ları hareket ettiriyoruz
             left++;
             right--;
 
